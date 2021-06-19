c3fcd33ae57923c4ef8d2de443efdbbfb50094ab
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-06-19 18:25:32 -0400

    made updates to the category section

--- ResturantAGoGo/ClientApp/src/app/category/category.component.ts
+++ ResturantAGoGo/ClientApp/src/app/category/category.component.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Restaurant } from '../Restaurant';
+import { RestaurantapiService } from '../restaurantapi.service';
 import { Router } from '@angular/router';
-import { DatastoreService } from '../services/datastore.service';
-import { RestaurantService } from '../services/restaurant.service';
+import { DatastoreService } from '../datastore.service';
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+    selector: 'app-category',
+    templateUrl: './category.component.html',
+    styleUrls: ['./category.component.css']
 })
+/** category component*/
 export class CategoryComponent {
 
+  /*restByCatList: Restaurant[] = [];*/
   zip_code: number = -1;
   categories: string = '';
 
-  constructor(private service: RestaurantService, public router: Router, public datastoreservice: DatastoreService) {
+    /** category ctor */
+  constructor(private service: RestaurantapiService, public router: Router, public datastoreservice: DatastoreService) {
 
   }
   setZipCode(form: NgForm): void {
     console.log(this.datastoreservice.getUser());
     this.zip_code = form.form.value.zip_code;
     console.log(this.zip_code);
-    if (this.zip_code == undefined) {
+    if (this.zip_code == undefined)
+    {
       this.zip_code = -1;
     }
     this.service.setZip(this.zip_code);
   }
+
   setCategoryClick(categories: string): void {
     this.categories = categories;
     console.log(this.categories);
