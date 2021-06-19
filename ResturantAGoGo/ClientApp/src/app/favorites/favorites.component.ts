e2503fcdf11680838f1aa085aed3b6826bf912db
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-06-19 19:00:09 -0400

    cleaned up the fav comp

---
+++ ResturantAGoGo/ClientApp/src/app/favorites/favorites.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DatastoreService } from '../datastore.service';
+import { Favorite } from '../Favorite';
+import { Restaurant } from '../Restaurant';
+import { RestaurantapiService } from '../restaurantapi.service';
+
+
+@Component({
+  selector: 'app-favorites',
+  templateUrl: './favorites.component.html',
+  styleUrls: ['./favorites.component.scss']
+})
+/** Favorites component*/
+export class FavoritesComponent {
+
+  
+  constructor(
+    private service: RestaurantapiService,
+    public router: Router,
+    private datastoreService: DatastoreService) { }
+
+  favList: Favorite[] = [];
+  restaurants: Restaurant[] = [];
+  favCategory: Restaurant[] = [];
+  usersName: string = "";
+
+  //need to fix the code
+
+  ngOnInit() {
+    this.service.getMyFavorites().subscribe(
+      (response: any) => {
+        this.favList = response.filter((f: Favorite)=> f.userId == this.datastoreService.getUser().userId);
+        console.log(this.favList);
+      }
+    )
+  }
+
+  addFavorite(restaurant: Restaurant) {
+    this.service.addFavorite(restaurant);
+  }
+
+  removeFavorite(favId: number, userId: number) {
+    this.service.removeFavorite(favId, userId);
+    let thisCategory = this.favList.find(e => e.favoriteId == favId);
+    let index = this.favList.indexOf(thisCategory);
+    this.favList.splice(index, 1);
+  }
+}
+
